05e5ab5e885321a8b93a85fb5e8d2bef4488b372
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2024-08-01 15:43:49 -0300

    0.0.5 -> 0.0.6, collect_orders

---
+++ fenicio/functionals/collect_orders.py
@@ -0,0 +1,31 @@
+from typing import List
+from fenicio import Fenicio, ParamsOrdersGET, FenicioOrders
+
+def download_all_orders_responses(fenicio: Fenicio, params: ParamsOrdersGET) -> List[dict]:
+    responses = []
+    flag = True
+    i = 1
+    while flag:
+        params.pag = i
+        r = fenicio.get_orders(params=params).json()   # TODO: Atajar caso `error=True`
+        if len(r["ordenes"]) == 0:
+            flag = False
+        else:
+            responses.append(r)
+            total_orders = sum(len(r["ordenes"]) for r in responses)
+            print(f"total_orders: {total_orders} | page: {i} | totAbs: {responses[-1]['totAbs']}")
+            i += 1
+    return responses
+
+def download_all_orders(fenicio: Fenicio, params: ParamsOrdersGET) -> FenicioOrders:
+    # FIXME: Hardcodeado, y no se controla un error=True, o si la requests falla.
+    responses = download_all_orders_responses(fenicio=fenicio, params=params)
+    orders = FenicioOrders(
+        error = False,
+        msj = "",
+        totAbs = responses[-1]["totAbs"],
+        ordenes = []
+    )
+    for r in responses:
+        orders.ordenes.extend(FenicioOrders(**r).ordenes)
+    return orders
